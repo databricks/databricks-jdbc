3c345135a06f1173ea1d5ac4172eb199d333113c
Vikrant Puppala <vikrant.puppala@databricks.com>
2024-01-12 16:38:16 +0530

    Added capture for logs

--- src/test/java/com/databricks/jdbc/local/OtelTester.java
+++ src/test/java/com/databricks/jdbc/local/OtelTester.java
@@ -1,32 +1,32 @@
 package com.databricks.jdbc.local;
 
-import static io.opentelemetry.semconv.ResourceAttributes.SERVICE_NAME;
-
-import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.LongHistogram;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import static io.opentelemetry.semconv.ResourceAttributes.SERVICE_NAME;
 
 public class OtelTester {
 
@@ -54,8 +54,20 @@ public class OtelTester {
                             .registerMetricReader(PeriodicMetricReader.builder(
                                     OtlpGrpcMetricExporter.getDefault())
                                     .setInterval(Duration.ofMillis(1000)).build())
-                            .build()).buildAndRegisterGlobal();
+                            .build())
+                    .setLoggerProvider(SdkLoggerProvider.builder()
+                            .setResource(resource)
+                            .addLogRecordProcessor(BatchLogRecordProcessor.builder(
+                                    OtlpGrpcLogRecordExporter.getDefault())
+                                    .build())
+                            .build())
+                    .buildAndRegisterGlobal();
             Runtime.getRuntime().addShutdownHook(new Thread(openTelemetrySdk::close));
+
+
+            io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender.install(
+                    openTelemetrySdk);
+
             return openTelemetrySdk;
 
 //            return GlobalOpenTelemetry.get();
@@ -63,19 +75,21 @@ public class OtelTester {
     }
 
 
-    private static final Logger LOGGER = LogManager.getLogger(OtelTester.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(OtelTester.class);
     static final AttributeKey<String> ATTR_METHOD = AttributeKey.stringKey("method");
 
     static final Random random = new Random();
 
     public static void main(String[] args) throws InterruptedException {
         OpenTelemetry openTelemetry = ExampleConfiguration.initOpenTelemetry();
+
+
         final Tracer tracer = openTelemetry.getTracer(ExampleConfiguration.class.getName());
         Meter meter = openTelemetry.getMeter(ExampleConfiguration.class.getName());
         final LongHistogram doWorkHistogram = meter.histogramBuilder("do-work").ofLongs().build();
 
         int sleepTime = random.nextInt(200);
-
+        LOGGER.info("Sleeping {}ms", sleepTime);
 
         Span span = tracer.spanBuilder("doWork").startSpan();
         try (Scope ignored = span.makeCurrent()) {
@@ -84,6 +98,7 @@ public class OtelTester {
         } finally {
             span.end();
         }
+        LOGGER.info("Work is done!");
 
         doWorkHistogram.record(sleepTime, Attributes.of(ATTR_METHOD, "ping"));
     }
