8007b031a2cd0250216d07c5391f419649cacefe
Madhav Sainanee <madhav.sainanee@databricks.com>
2024-03-13 11:16:08 +0530

    formatting

--- src/test/java/com/databricks/jdbc/integration/datatypes/DataTypesIntegrationTests.java
+++ src/test/java/com/databricks/jdbc/integration/datatypes/DataTypesIntegrationTests.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
