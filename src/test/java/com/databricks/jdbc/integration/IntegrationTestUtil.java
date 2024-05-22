4a96d65ac1c6db36c472e1dd3200a175966537f4
Samikshya Chand <samikshya.chand@databricks.com>
2024-05-22 11:21:21 +0530

    Add fix for fake service

--- src/test/java/com/databricks/jdbc/integration/IntegrationTestUtil.java
+++ src/test/java/com/databricks/jdbc/integration/IntegrationTestUtil.java
@@ -90,7 +90,7 @@ public class IntegrationTestUtil {
     String template =
         Boolean.parseBoolean(System.getProperty(IS_FAKE_SERVICE_TEST_PROP))
             ? "jdbc:databricks://%s/default;transportMode=http;ssl=1;AuthMech=3;httpPath=%s"
-            : "jdbc:databricks://%s/default;ssl=1;AuthMech=3;httpPath=%s";
+            : "jdbc:databricks://%s/default;ssl=0;AuthMech=3;httpPath=%s";
 
     String host = getDatabricksHost();
     String httpPath = getDatabricksHTTPPath();
