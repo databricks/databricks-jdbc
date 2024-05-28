d48c46d44adcad45ad452904750cd6071e25fe93
Agnipratim Nag <agnipratim.nag@databricks.com>
2024-05-28 16:39:52 +0530

    Addressed Madhav and Samikshya's comments. Added a case sensitive toggle for the prefixExists function.

--- src/main/java/com/databricks/jdbc/client/impl/sdk/DatabricksUCVolumeClient.java
+++ src/main/java/com/databricks/jdbc/client/impl/sdk/DatabricksUCVolumeClient.java
@@ -15,24 +15,36 @@ public class DatabricksUCVolumeClient implements IDatabricksUCVolumeClient {
   public DatabricksUCVolumeClient(Connection connection) {
     this.connection = connection;
   }
+  private String createListQuery(String catalog, String schema, String volume) {
+    return String.format("LIST '/Volumes/%s/%s/%s/'", catalog, schema, volume);
+  }
 
   @Override
   public boolean prefixExists(
-      String catalog, String schema, String volume, String prefix, Statement statement)
-      throws SQLException {
+          String catalog, String schema, String volume, String prefix, boolean caseSensitive)
+          throws SQLException {
 
-    String listFilesSQL = "LIST '/Volumes/" + catalog + "/" + schema + "/" + volume + "/'";
+    String listFilesSQLQuery = createListQuery(catalog, schema, volume);
 
-    ResultSet resultSet = statement.executeQuery(listFilesSQL);
+    try (Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery(listFilesSQLQuery);
 
-    boolean exists = false;
-    while (resultSet.next()) {
-      String fileName = resultSet.getString("name");
-      if (fileName.startsWith(prefix)) {
-        exists = true;
-        break;
+      boolean exists = false;
+      while (resultSet.next()) {
+        String fileName = resultSet.getString("name");
+        if (caseSensitive) {
+          if (fileName.startsWith(prefix)) {
+            exists = true;
+            break;
+          }
+        } else {
+          if (fileName.toLowerCase().startsWith(prefix.toLowerCase())) {
+            exists = true;
+            break;
+          }
+        }
       }
+      return exists;
     }
-    return exists;
   }
 }
--- src/test/java/com/databricks/jdbc/core/DatabricksUCVolumeClientTest.java
+++ src/test/java/com/databricks/jdbc/core/DatabricksUCVolumeClientTest.java
@@ -2,7 +2,7 @@ package com.databricks.jdbc.core;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+import static com.databricks.jdbc.TestConstants.*;
 import com.databricks.jdbc.client.impl.sdk.DatabricksUCVolumeClient;
 import java.sql.*;
 import java.util.stream.Stream;
@@ -17,10 +17,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class DatabricksUCVolumeClientTest {
 
-  private static final String CATALOG = "samikshya_hackathon";
-
-  private static final String SCHEMA = "agnipratim_test";
-
   @Mock Connection connection;
 
   @Mock Statement statement;
@@ -29,65 +25,51 @@ public class DatabricksUCVolumeClientTest {
 
   @Test
   public void testPrefixExists() throws SQLException {
-    // Arrange
-    // DatabricksUCVolumeSdkClient client = new DatabricksUCVolumeSdkClient(JDBC_URL, USER,
-    // PASSWORD, CON, STATEMENT);
+
     String volume = "abc_volume1";
     String prefix = "efg";
 
     DatabricksUCVolumeClient client = new DatabricksUCVolumeClient(connection);
 
-    // Define the behavior of the mock objects
     when(connection.createStatement()).thenReturn(statement);
-    String listFilesSQL = "LIST '/Volumes/" + CATALOG + "/" + SCHEMA + "/" + volume + "/'";
+    String listFilesSQL = "LIST '/Volumes/" + TEST_CATALOG + "/" + TEST_SCHEMA + "/" + volume + "/'";
     when(statement.executeQuery(listFilesSQL)).thenReturn(resultSet);
     when(resultSet.next())
         .thenReturn(true, true, true, true, false); // Simulate four rows in the result set
     when(resultSet.getString("name"))
         .thenReturn("abc_file1", "abc_file2", "def_file1", "efg_file2");
 
-    // Act
-    Statement mock_statement = connection.createStatement();
-    boolean exists = client.prefixExists(CATALOG, SCHEMA, volume, prefix, mock_statement);
+    boolean exists = client.prefixExists(TEST_CATALOG, TEST_SCHEMA, volume, prefix, true);
 
-    // Assert
     assertTrue(exists);
     verify(statement)
-        .executeQuery("LIST '/Volumes/" + CATALOG + "/" + SCHEMA + "/" + volume + "/'");
-    // verify(resultSet, times(4)).next(); // Verify that next() is called four times
-    // parametrised test
-
+        .executeQuery("LIST '/Volumes/" + TEST_CATALOG + "/" + TEST_SCHEMA + "/" + volume + "/'");
   }
 
   @ParameterizedTest
   @MethodSource("provideParameters")
   public void testPrefixExists(String volume, String prefix, boolean expected) throws SQLException {
-    // Arrange
     DatabricksUCVolumeClient client = new DatabricksUCVolumeClient(connection);
 
-    // Define the behavior of the mock objects
     when(connection.createStatement()).thenReturn(statement);
-    String listFilesSQL = "LIST '/Volumes/" + CATALOG + "/" + SCHEMA + "/" + volume + "/'";
+    String listFilesSQL = "LIST '/Volumes/" + TEST_CATALOG + "/" + TEST_SCHEMA + "/" + volume + "/'";
     when(statement.executeQuery(listFilesSQL)).thenReturn(resultSet);
     when(resultSet.next())
-        .thenReturn(true, true, true, true, false); // Simulate four rows in the result set
+        .thenReturn(true, true, true, true, true, false); // Simulate four rows in the result set
     when(resultSet.getString("name"))
-        .thenReturn("abc_file1", "abc_file2", "def_file1", "efg_file2");
+        .thenReturn("aBc_file1", "abC_file2", "def_file1", "efg_file2", "#!#_file3");
 
-    // Act
-    Statement mock_statement = connection.createStatement();
-    boolean exists = client.prefixExists(CATALOG, SCHEMA, volume, prefix, mock_statement);
+    boolean exists = client.prefixExists(TEST_CATALOG, TEST_SCHEMA, volume, prefix, true);
 
-    // Assert
     assertEquals(expected, exists);
     verify(statement)
-        .executeQuery("LIST '/Volumes/" + CATALOG + "/" + SCHEMA + "/" + volume + "/'");
+        .executeQuery("LIST '/Volumes/" + TEST_CATALOG + "/" + TEST_SCHEMA + "/" + volume + "/'");
   }
 
   private static Stream<Arguments> provideParameters() {
     return Stream.of(
-        Arguments.of("abc_volume1", "efg", true), Arguments.of("abc_volume2", "xyz", false)
-        // Add more sets of parameters as needed
+        Arguments.of("abc_volume1", "abc", false), Arguments.of("abc_volume2", "xyz", false), Arguments.of("abc_volume2", "def", true),
+        Arguments.of("abc_volume2", "#!", true), Arguments.of("abc_volume2", "aBc", true)
         );
   }
 }
