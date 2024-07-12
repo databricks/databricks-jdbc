03a9187cf434ab5f0b16ab1602562a0161a36ba6
Jothi Prakash <jothi.prakash@databricks.com>
2024-07-12 11:29:53 +0530

    Fixed Formatting issues

--- src/main/java/com/databricks/client/jdbc/Driver.java
+++ src/main/java/com/databricks/client/jdbc/Driver.java
@@ -42,7 +42,6 @@ public class Driver implements java.sql.Driver {
 
   @Override
   public Connection connect(String url, Properties info) throws DatabricksSQLException {
-
     IDatabricksConnectionContext connectionContext = DatabricksConnectionContext.parse(url, info);
 
     setUserAgent(connectionContext);
