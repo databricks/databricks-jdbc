ba0f3b53b05866c14cb68f3de5d8f823de16823d
Jayant Singh <jayant.singh@databricks.com>
2024-08-06 12:35:04 +0530

    Fix connection integration tests to use default catalog
    Some cosmetic changes as well

--- src/test/java/com/databricks/jdbc/integration/fakeservice/tests/UCVolumeDataExchangeIntegrationTests.java
+++ src/test/java/com/databricks/jdbc/integration/fakeservice/tests/UCVolumeDataExchangeIntegrationTests.java
@@ -40,12 +40,14 @@ public class UCVolumeDataExchangeIntegrationTests extends AbstractFakeServiceInt
           "https://us-west-2-extstaging-managed-catalog-test-bucket-1.s3-fips.us-west-2.amazonaws.com");
 
   private DatabricksUCVolumeClient client;
+
   private Connection con;
 
   private static final String jdbcUrlTemplate =
       "jdbc:databricks://%s/default;transportMode=http;ssl=0;AuthMech=3;httpPath=%s;catalog=SPARK";
 
   private static final String HTTP_PATH = "/sql/1.0/warehouses/791ba2a31c7fd70a";
+
   private static final String LOCAL_TEST_DIRECTORY = "/tmp";
 
   @BeforeAll
--- src/test/java/com/databricks/jdbc/integration/fakeservice/tests/UCVolumeIntegrationTests.java
+++ src/test/java/com/databricks/jdbc/integration/fakeservice/tests/UCVolumeIntegrationTests.java
@@ -24,12 +24,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class UCVolumeIntegrationTests extends AbstractFakeServiceIntegrationTests {
 
   private DatabricksUCVolumeClient client;
+
   private Connection con;
 
   private static final String jdbcUrlTemplate =
       "jdbc:databricks://%s/default;transportMode=http;ssl=0;AuthMech=3;httpPath=%s;catalog=SPARK";
 
   private static final String HTTP_PATH = "/sql/1.0/warehouses/791ba2a31c7fd70a";
+
   private static final String LOCAL_TEST_DIRECTORY = "/tmp";
 
   @BeforeAll
