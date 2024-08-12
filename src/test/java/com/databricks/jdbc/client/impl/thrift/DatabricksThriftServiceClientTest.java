43903660e63337aec11e6f4e4154d460e6a3b323
Madhav Sainanee <madhav.sainanee@databricks.com>
2024-08-12 14:20:34 +0530

    fmt

--- src/test/java/com/databricks/jdbc/client/impl/thrift/DatabricksThriftServiceClientTest.java
+++ src/test/java/com/databricks/jdbc/client/impl/thrift/DatabricksThriftServiceClientTest.java
@@ -22,7 +22,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
