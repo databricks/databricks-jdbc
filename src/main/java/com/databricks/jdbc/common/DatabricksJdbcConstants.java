4e784ae53ff40f1641e2cb86889856b03b2f1e3d
Gopal Lal <gopal.lal@databricks.com>
2024-09-12 20:30:05 +0530

    Fixes

--- src/main/java/com/databricks/jdbc/common/DatabricksJdbcConstants.java
+++ src/main/java/com/databricks/jdbc/common/DatabricksJdbcConstants.java
@@ -50,7 +50,6 @@ public final class DatabricksJdbcConstants {
   public static final String ALLOWED_VOLUME_INGESTION_PATHS =
       "allowlistedVolumeOperationLocalFilePaths";
   public static final String ALLOWED_STAGING_INGESTION_PATHS = "StagingAllowedLocalPaths";
-
   public static final String VOLUME_OPERATION_STATUS_COLUMN_NAME = "operation_status";
   public static final String VOLUME_OPERATION_STATUS_SUCCEEDED = "SUCCEEDED";
   public static final Map<String, String> ALLOWED_SESSION_CONF_TO_DEFAULT_VALUES_MAP =
