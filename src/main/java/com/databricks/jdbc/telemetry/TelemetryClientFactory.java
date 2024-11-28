597cacfaed2a81b42d88eb92174e6f0cf68525d5
Gopal Lal <gopal.lal@databricks.com>
2024-11-28 15:13:38 +0530

    Add client

--- src/main/java/com/databricks/jdbc/telemetry/TelemetryClientFactory.java
+++ src/main/java/com/databricks/jdbc/telemetry/TelemetryClientFactory.java
@@ -2,11 +2,13 @@ package com.databricks.jdbc.telemetry;
 
 import com.databricks.jdbc.api.IDatabricksConnectionContext;
 import com.databricks.jdbc.api.impl.DatabricksConnection;
+import com.databricks.jdbc.dbclient.impl.http.DatabricksHttpClient;
 import com.databricks.jdbc.exception.DatabricksParsingException;
 import com.databricks.jdbc.log.JdbcLogger;
 import com.databricks.jdbc.log.JdbcLoggerFactory;
 import com.databricks.jdbc.model.telemetry.TelemetryEvent;
 
+import java.io.IOException;
 import java.util.LinkedHashMap;
 
 public class TelemetryClientFactory {
@@ -16,21 +18,18 @@ public class TelemetryClientFactory {
     static LinkedHashMap<String, TelemetryClient> telemetryClients = new LinkedHashMap<>();
 
     public static ITelemetryClient getTelemetryClient(IDatabricksConnectionContext connectionContext) {
-        String hostUrl;
-        try {
-            hostUrl = connectionContext.getHostUrl();
-        } catch (DatabricksParsingException e) {
-            // We should not throw exception for Telemetry flow, this should be non-blocking for regular JDBC
-            // operations. Though very likely the exception should have already thrown by now
-            logger.error(e, "Failed to initialize Telemetry client due to parsing error {}", e.getMessage());
-            return new NoopTelemetryClient();
-        }
-        if (telemetryClients.containsKey(hostUrl)) {
-            return telemetryClients.get(hostUrl);
-        } else {
-            TelemetryClient client = new TelemetryClient(connectionContext);
-            telemetryClients.put(hostUrl, client);
-            return client;
+        return telemetryClients.computeIfAbsent(
+                connectionContext.getConnectionUuid(), k -> new TelemetryClient(connectionContext));
+    }
+
+    public static void closeTelemetryClient(IDatabricksConnectionContext connectionContext) {
+        ITelemetryClient instance = telemetryClients.remove(connectionContext.getConnectionUuid());
+        if (instance != null) {
+            try {
+                instance.close();
+            } catch (Exception e) {
+                logger.debug(String.format("Caught error while closing telemetry client. Error %s", e));
+            }
         }
     }
 }
