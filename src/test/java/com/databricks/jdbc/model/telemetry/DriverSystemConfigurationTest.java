045c72cd2456ab746203198c1181078b28406fb0
Samikshya Chand <samikshya.chand@databricks.com>
2024-12-17 13:35:54 +0530

    Add tests

--- src/test/java/com/databricks/jdbc/model/telemetry/DriverSystemConfigurationTest.java
+++ src/test/java/com/databricks/jdbc/model/telemetry/DriverSystemConfigurationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class DriverSystemConfigurationUtilTest {
+public class DriverSystemConfigurationTest {
   @Test
   public void testGetSystemProperties() {
     assertDoesNotThrow(() -> DriverSystemConfiguration.getInstance().toString());
