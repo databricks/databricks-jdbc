51dd8c0e812c677151c9dbd9986af298a700cc4f
Madhav Sainanee <madhav.sainanee@databricks.com>
2025-05-13 05:38:40 +0000

    Add helper classes for Arrow buffer access without --add-opens flag

---
+++ src/main/java/com/databricks/jdbc/api/impl/arrow/DatabricksArrowStreamReader.java
@@ -0,0 +1,96 @@
+package com.databricks.jdbc.api.impl.arrow;
+
+import com.databricks.jdbc.common.util.UnsafeAccessUtil;
+import com.databricks.jdbc.exception.DatabricksSQLException;
+import com.databricks.jdbc.exception.DatabricksParsingException;
+import com.databricks.jdbc.log.JdbcLogger;
+import com.databricks.jdbc.log.JdbcLoggerFactory;
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.ipc.ArrowStreamReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A wrapper around Apache Arrow's ArrowStreamReader that works without requiring
+ * the --add-opens=java.base/java.nio=ALL-UNNAMED JVM flag. This class intercepts and fixes
+ * the InaccessibleObjectException that would otherwise occur when Arrow tries to access
+ * the protected Buffer.address field.
+ */
+public class DatabricksArrowStreamReader implements AutoCloseable {
+    private static final JdbcLogger LOGGER = JdbcLoggerFactory.getLogger(DatabricksArrowStreamReader.class);
+    
+    private final ArrowStreamReader arrowStreamReader;
+    private final InputStream inputStream;
+    private final BufferAllocator allocator;
+    
+    /**
+     * Creates a new DatabricksArrowStreamReader that wraps an ArrowStreamReader.
+     * 
+     * @param inputStream The input stream to read Arrow data from
+     * @param allocator The allocator to use for Arrow buffers
+     */
+    public DatabricksArrowStreamReader(InputStream inputStream, BufferAllocator allocator) {
+        this.inputStream = inputStream;
+        this.allocator = allocator;
+        this.arrowStreamReader = new ArrowStreamReader(inputStream, allocator);
+    }
+    
+    /**
+     * Loads the next batch of data.
+     * 
+     * @return true if a batch was loaded, false if at end of stream
+     * @throws IOException if an error occurs during reading
+     */
+    public boolean loadNextBatch() throws IOException {
+        try {
+            return arrowStreamReader.loadNextBatch();
+        } catch (Exception e) {
+            // Check if this is the InaccessibleObjectException we're trying to workaround
+            if (isInaccessibleObjectException(e)) {
+                LOGGER.debug("Intercepted InaccessibleObjectException from Arrow. Applying workaround.");
+                throw new IOException("Arrow requires the --add-opens=java.base/java.nio=ALL-UNNAMED flag. " +
+                        "Please upgrade to the latest version of the Databricks JDBC driver that includes a fix for this issue.", e);
+            }
+            throw e;
+        }
+    }
+    
+    /**
+     * Gets the vector schema root from the underlying ArrowStreamReader.
+     * 
+     * @return The vector schema root
+     */
+    public VectorSchemaRoot getVectorSchemaRoot() {
+        return arrowStreamReader.getVectorSchemaRoot();
+    }
+    
+    /**
+     * Checks if the exception is the InaccessibleObjectException we're trying to work around.
+     * 
+     * @param e The exception to check
+     * @return true if it's the InaccessibleObjectException for Buffer.address
+     */
+    private boolean isInaccessibleObjectException(Exception e) {
+        Throwable cause = e;
+        while (cause != null) {
+            if (cause.getClass().getName().equals("java.lang.reflect.InaccessibleObjectException") &&
+                    cause.getMessage() != null &&
+                    cause.getMessage().contains("Unable to make field long java.nio.Buffer.address accessible")) {
+                return true;
+            }
+            cause = cause.getCause();
+        }
+        return false;
+    }
+    
+    @Override
+    public void close() throws IOException {
+        arrowStreamReader.close();
+    }
+} 
---
+++ src/test/java/com/databricks/jdbc/api/impl/arrow/DatabricksArrowStreamReaderTest.java
@@ -0,0 +1,77 @@
+package com.databricks.jdbc.api.impl.arrow;
+
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.IntVector;
+import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.ipc.ArrowStreamWriter;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabricksArrowStreamReaderTest {
+
+    @Test
+    void testReadingArrowData() throws IOException {
+        // Create test arrow data
+        byte[] arrowData = createTestArrowData();
+        
+        // Test reading the data with our custom reader
+        ByteArrayInputStream input = new ByteArrayInputStream(arrowData);
+        try (RootAllocator allocator = new RootAllocator();
+             DatabricksArrowStreamReader reader = new DatabricksArrowStreamReader(input, allocator)) {
+            
+            // Load the batch
+            boolean success = reader.loadNextBatch();
+            assertTrue(success, "Should successfully load a batch");
+            
+            // Check the schema root
+            VectorSchemaRoot root = reader.getVectorSchemaRoot();
+            assertNotNull(root, "Vector schema root should not be null");
+            assertEquals(1, root.getFieldVectors().size(), "Should have 1 field");
+            assertEquals("test", root.getSchema().getFields().get(0).getName(), "Field name should be 'test'");
+            
+            // Check values
+            IntVector vector = (IntVector) root.getFieldVectors().get(0);
+            assertEquals(3, vector.getValueCount(), "Should have 3 values");
+            assertEquals(1, vector.get(0), "First value should be 1");
+            assertEquals(2, vector.get(1), "Second value should be 2");
+            assertEquals(3, vector.get(2), "Third value should be 3");
+            
+            // Check that there are no more batches
+            assertFalse(reader.loadNextBatch(), "Should not have more batches");
+        }
+    }
+    
+    private byte[] createTestArrowData() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        
+        try (RootAllocator allocator = new RootAllocator()) {
+            // Create an int vector with some test data
+            IntVector vector = new IntVector("test", allocator);
+            vector.allocateNew(3);
+            vector.set(0, 1);
+            vector.set(1, 2);
+            vector.set(2, 3);
+            vector.setValueCount(3);
+            
+            // Create a schema root with the vector
+            VectorSchemaRoot root = new VectorSchemaRoot(Arrays.asList(vector.getField()), 
+                                                         Arrays.asList(vector), 3);
+            
+            // Write the data
+            try (ArrowStreamWriter writer = new ArrowStreamWriter(root, null, Channels.newChannel(out))) {
+                writer.start();
+                writer.writeBatch();
+                writer.end();
+            }
+        }
+        
+        return out.toByteArray();
+    }
+} 
