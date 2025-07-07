e60992163b6b756673d864c7eb30094a8e93e8bb
Jayant Singh <jayant.singh@databricks.com>
2025-07-07 17:01:06 +0530

    Remove secrets from the FakeServiceTests

--- src/test/java/com/databricks/jdbc/integration/IntegrationTestUtil.java
+++ src/test/java/com/databricks/jdbc/integration/IntegrationTestUtil.java
@@ -193,8 +193,6 @@ public class IntegrationTestUtil {
 
   public static Connection getValidJDBCConnection() throws SQLException {
     Properties connectionProperties = new Properties();
-    connectionProperties.put(DatabricksJdbcUrlParams.USER.getParamName(), getDatabricksUser());
-    connectionProperties.put(DatabricksJdbcUrlParams.PASSWORD.getParamName(), getDatabricksToken());
     connectionProperties.put(
         DatabricksJdbcUrlParams.ENABLE_SQL_EXEC_HYBRID_RESULTS.getParamName(), '0');
 
@@ -208,11 +206,17 @@ public class IntegrationTestUtil {
       connectionProperties.put(
           DatabricksJdbcUrlParams.USE_THRIFT_CLIENT.getParamName(),
           FakeServiceConfigLoader.shouldUseThriftClient());
+      connectionProperties.put(DatabricksJdbcUrlParams.USER.getParamName(), "token");
+      connectionProperties.put(DatabricksJdbcUrlParams.PASSWORD.getParamName(), "token");
 
       return DriverManager.getConnection(getFakeServiceJDBCUrl(), connectionProperties);
-    }
+    } else {
+      connectionProperties.put(DatabricksJdbcUrlParams.USER.getParamName(), getDatabricksUser());
+      connectionProperties.put(
+          DatabricksJdbcUrlParams.PASSWORD.getParamName(), getDatabricksToken());
 
-    return DriverManager.getConnection(getJDBCUrl(), connectionProperties);
+      return DriverManager.getConnection(getJDBCUrl(), connectionProperties);
+    }
   }
 
   public static Connection getValidJDBCConnection(List<List<String>> extraArgs)
