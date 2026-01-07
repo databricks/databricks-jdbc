0e4395940e4f2f91b51701882a797494a68234b8
samikshya-chand_data <samikshya.chand@databricks.com>
2026-01-07 12:01:03 +0530

    missed adding a file

--- src/main/java/com/databricks/jdbc/telemetry/TelemetryClientFactory.java
+++ src/main/java/com/databricks/jdbc/telemetry/TelemetryClientFactory.java
@@ -19,6 +19,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TelemetryClientFactory {
 
